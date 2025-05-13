0e1ca7e68620c6ee55eab5f6f01fe8c7dbfd83f1
[julianlorenteiesnervion] <[julian.lorente@iesnervion.es]>
2025-05-13 14:37:22 +0200

    Ejercicio 1 NO terminado.

--- src/boletin1/Ejer1.java
+++ src/boletin1/Ejer1.java
@@ -1,9 +1,33 @@
 package boletin1;
 
+import java.sql.*;
+import java.util.*;
+
 public class Ejer1 {
 
 	public static void main(String[] args) {
+		
+		Scanner reader = new Scanner(System.in);
 
+		String url = "jdbc:mysql://localhost/institutodb";
+		String usuario = "root";
+		String pswd;
+		
+		String consulta;
+		
+		System.out.print("Introduce la contraseña: ");
+		pswd = reader.nextLine();
+		
+		try (Connection con = DriverManager.getConnection(url, usuario, pswd)) {
+			System.out.println("Credenciales correctas.\nConectado a la base de datos.");
+			
+			Sta
+			
+		} catch (SQLException e) {
+			System.out.println("Error al establecer la conexión con la base de datos: " + e);
+		}
+		
+		reader.close();
 		
 	}
 
