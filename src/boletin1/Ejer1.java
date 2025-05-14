57e5432e4b78ce6f50628f91106255b37db7539a
[julianlorenteiesnervion] <[julian.lorente@iesnervion.es]>
2025-05-14 08:38:41 +0200

    Ejercicio 1 terminado.

--- src/boletin1/Ejer1.java
+++ src/boletin1/Ejer1.java
@@ -21,7 +21,13 @@ public class Ejer1 {
 		try (Connection con = DriverManager.getConnection(url, usuario, pswd)) {
 			System.out.println("Credenciales correctas.\nConectado a la base de datos.");
 			
-			Sta
+			Statement sentencia = con.createStatement();
+			
+			consulta = "INSERT INTO Estudiantes (nombre, apellido, fecha_nacimiento, email, telefono) VALUES ('Julián', 'Lorente', '2006-11-14', 'julian.lorente@iesnervion.es', '654321987')";
+			sentencia.executeUpdate(consulta);
+			
+			consulta = "INSERT INTO Estudiantes (nombre, apellido, fecha_nacimiento, email, telefono) VALUES ('José Manuel', 'Maya', '2005-09-15', 'jm.maya@iesnervion.es', '732144988')";
+			sentencia.executeUpdate(consulta);
 			
 		} catch (SQLException e) {
 			System.out.println("Error al establecer la conexión con la base de datos: " + e);
