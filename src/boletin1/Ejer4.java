ee292c8c1369047b21f81b3a8da2ec3442b24abc
[julianlorenteiesnervion] <[julian.lorente@iesnervion.es]>
2025-05-14 09:18:31 +0200

    Ejercicio 4 hecho de otra manera.

--- src/boletin1/Ejer4.java
+++ src/boletin1/Ejer4.java
@@ -16,30 +16,17 @@ public class Ejer4 {
 		String usuario = "root";
 		String pswd;
 		
-		String consulta;
-		
 		System.out.print("Introduce la contraseña: ");
 		pswd = reader.nextLine();
 		
 		try (Connection con = DriverManager.getConnection(url, usuario, pswd)) {
 			System.out.println("Credenciales correctas.\nConectado a la base de datos.");
 			
-			Statement sentencia = con.createStatement();
-
-			consulta = "INSERT INTO Cursos (nombre, descripcion, año_escolar) VALUES ('Historia 1º', 'Curso de Historia para el primer año', 2025)";
-			sentencia.executeUpdate(consulta);
-			
-			consulta = "INSERT INTO Cursos (nombre, descripcion, año_escolar) VALUES ('Biología 1º', 'Curso de Biología para el primer año', 2025)";
-			sentencia.executeUpdate(consulta);
-			
-			consulta = "INSERT INTO Cursos (nombre, descripcion, año_escolar) VALUES ('Educación Física 1º', 'Curso de Educación Física para el primer año', 2025)";
-			sentencia.executeUpdate(consulta);
-			
-			consulta = "INSERT INTO Cursos (nombre, descripcion, año_escolar) VALUES ('Música 1º', 'Curso de Música para el primer año', 2025)";
-			sentencia.executeUpdate(consulta);
-			
-			consulta = "INSERT INTO Cursos (nombre, descripcion, año_escolar) VALUES ('Tecnología 1º', 'Curso de Tecnología para el primer año', 2025)";
-			sentencia.executeUpdate(consulta);
+			anadirCurso(con, "Historia");
+			anadirCurso(con, "Biología");
+			anadirCurso(con, "Educación Física");
+			anadirCurso(con, "Música");
+			anadirCurso(con, "Tecnología");
 			
 		} catch (SQLException e) {
 			System.out.println("Error al establecer la conexión con la base de datos: " + e);
@@ -47,6 +34,24 @@ public class Ejer4 {
 		
 		reader.close();
 		
+	}
+	
+	static private void anadirCurso(Connection con, String curso) {
+		Statement sentencia = null;
+		
+		try {
+			sentencia = con.createStatement();
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+		}
+		
+		String consulta = "INSERT INTO Cursos (nombre, descripcion, año_escolar) VALUES ('" + curso + " 1º', 'Curso de " + curso + " para el primer año', 2025)";
+		
+		try {
+			sentencia.executeUpdate(consulta);
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+		}
 	}
 
 }
